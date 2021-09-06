ab222573000208f525ead19b7da848713d22dec2
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-09-06 13:13:49 -0300

    Page SignIn, Componente InputCustom, Create Header, Componente SideBar, Page Dashboard

---
+++ src/components/Header.tsx
@@ -0,0 +1,89 @@
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine, RiUserLine } from 'react-icons/ri'
+
+
+export function Header() {
+  return (
+    <Flex
+      w="100%"
+      as="header"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      px="6"
+      align="center"
+    >
+      <Text
+        fontSize="3xl"
+        fontWeight="bold"
+        letterSpacing="tight"
+        w="64"
+      >
+        dashgo
+        <Text as="span" ml="1" color="pink.500">.</Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Buscar na Plataforma"
+          _placeholder={{ color: 'gray.400' }}
+        />
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          // borderRadiusWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Luis Felipe</Text>
+            <Text 
+            color="gray.300" 
+            fontSize="small"
+            >
+              felipe-mara2003@hotmail.com
+            </Text>
+          </Box>
+
+          <Avatar 
+            size="md" 
+            src="https://avatars.githubusercontent.com/u/78617974?v=4" 
+            name="Luis Felipe" 
+          />
+          
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,49 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { InputCustom } from '../components/Form/InputCustom'
 
-
-export default function Home() {
+export default function SignIn() {
   return (
-    <div>DashGo</div>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+
+      >
+        <Stack spacing="4">
+          <InputCustom
+            name="email"
+            label="E-mail"
+            type="email"
+          />
+
+
+          <InputCustom
+            name="password"
+            label="Senha"
+            type="password"
+          />
+
+
+        </Stack>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
